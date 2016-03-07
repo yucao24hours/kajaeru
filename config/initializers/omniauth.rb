f2aa4356e364d6ae16390713a7e8a912a0fa05e1
Hirofumi Wakasugi <baenej@gmail.com>
2016-03-08 00:51:42 +0900

    Mock OmniAuth to skip actual authentication in development and test

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,13 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  if Rails.env.development? || Rails.env.test?
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(
+      provider: :github,
+      nickname: 'alice',
+      uid: '11111',
+      image: 'http://pic.prepics-cdn.com/9ece34e247cc4/54052408.jpeg'
+    )
+  end
+
   provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET']
 end
